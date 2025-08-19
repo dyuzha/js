27f1af6000cc64d6e1d8c34e9617a1603ef68dc9
dyuzha <matvey10d@gmail.com>
2025-08-19 16:54:36 +0300

    ..

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 // 'use strict';
 
-let name = prompt("input you name", "NoName");
 
-alert(`Hi ${name}`)
-
+outer: for (let i = 0; i < 3; i++) {
+    for (let j= 0; i < 3; j++) {
+        let input = prompt(`Coord. Value: ${i};${j}`, "");
+    }   if (!input) break outer;
+        // Действие
+    break
+}
