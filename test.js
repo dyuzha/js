6124dfbeae4b57258e4477099dea316456757de3
dyuzha <matvey10d@gmail.com>
2025-08-20 16:52:42 +0300

    ..

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 // 'use strict';
 
-
-outer: for (let i = 0; i < 3; i++) {
-    for (let j= 0; i < 3; j++) {
-        let input = prompt(`Coord. Value: ${i};${j}`, "");
-    }   if (!input) break outer;
-        // Действие
-    break
+/**
+ * @param {string} from
+ * @param {string} text
+ */
+function showMessage(from, text = "No text") {
+    alert(`${from}: ${text}`);
 }
+
+showMessage("Anna", "Hi");
+showMessage("Anna");
