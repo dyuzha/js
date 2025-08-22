e1fee4f5c9e1ff7e7a7e15f57ed190143f7761c6
dyuzha <matvey10d@gmail.com>
2025-08-22 19:05:23 +0300

    ..

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 // 'use strict';
 
-import { assert } from "chai";
+// import { assert } from "chai";
 
 describe("pow", () => {
 
   it("возводит в степень n", () => {
     assert.equal(pow(2, 3), 8);
+    assert.equal(pow(2, 2), 4);
   });
 
 });
